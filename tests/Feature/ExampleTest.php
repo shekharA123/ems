b131566c2fc58d8f8d521e1fa6fea942f6b19cfa
Naresh Upadhyay <naresh.king88898@gmail.com>
2023-09-23 13:19:05 +0530

    corrected test cases

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,15 +4,15 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class ExampleTestFalse extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_that_true_is_true()
+    public function test_that_false_is_true()
     {
-        $this->assertTrue(true);
+        $this->assertFalse(false);
     }
 }
