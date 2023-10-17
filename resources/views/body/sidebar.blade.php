d13a9298f3ba6e12821d6337e42cc8f4052815d1
PraviniM <pravini1105@gmail.com>
2023-10-17 19:10:30 +0530

    cal changes

--- resources/views/body/sidebar.blade.php
+++ resources/views/body/sidebar.blade.php
@@ -36,11 +36,10 @@
 
                         <ul id="side-menu">
 
-                            <!-- <li class="menu-title">Navigation</li> -->
 
           <li>
             <a href="{{ url('/dashboard') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+              
                <img src="backend/assets/images/Union.svg" alt="">
                 <span style="margin-left:7px;" > Dashboard </span>
             </a>
@@ -49,8 +48,6 @@
 
            <li>
             <a href="{{ route('pos') }}">
-                <!-- <span class="badge bg-pink float-end">Hot</span> -->
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
                <img src="backend/assets/images/clarity_inbox-line.svg" alt="">
                 <span style="margin-left:5px;"> Inbox </span>
             </a>
@@ -58,7 +55,6 @@
 
         <li>
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
                <img src="backend/assets/images/Calendar.svg" alt="">
                 <span style="margin-left:5px;"> Calendar & todo’s </span>
             </a>
@@ -68,29 +64,9 @@
 
         <li class="nav-header recruitment mt-2" style="margin-left:23px;" >RECRUITMENT</li>
 
-
-
-                            <!-- <li>
-                                <a href="#sidebarEcommerce" data-bs-toggle="collapse">
-                                    <i class="mdi mdi-cart-outline mt-2"></i>
-                                    <span> Jobs  </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <div class="collapse" id="sidebarEcommerce">
-                                    <ul class="nav-second-level">
-                                        <li>
-                                            <a href="{{ route('all.employee') }}">All Employee</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ route('add.employee') }}">Add Employee </a>
-                                        </li>
-
-                                    </ul>
-                                </div>
-                            </li> -->
                             <li class="mt-2">
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+            
                <img src="backend/assets/images/Bag 2.svg" alt="">
                 <span style="margin-left:5px;"> Jobs </span>
             </a>
@@ -98,7 +74,7 @@
 
         <li>
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+              
                <img src="backend/assets/images/octicon_people-24.svg" alt="">
                 <span style="margin-left:5px;"> Candidates  </span>
             </a>
@@ -107,7 +83,7 @@
 
         <li>
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+             
                <img src="backend/assets/images/ph_triangle-bold.svg" alt="">
                 <span style="margin-left:5px;"> My referrals </span>
             </a>
@@ -116,7 +92,7 @@
 
         <li>
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+            
                <img src="backend/assets/images/fluent-mdl2_site-scan.svg" alt="">
                 <span style="margin-left:5px;"> Career site </span>
             </a>
@@ -126,7 +102,7 @@
 
         <li class="mt-2">
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+              
                <img src="backend/assets/images/employee.svg" alt="">
                 <span style="margin-left:5px;"> Employee </span>
             </a>
@@ -134,7 +110,7 @@
 
         <li>
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+            
                <img src="backend/assets/images/ph_tree-structure-fill.svg" alt="">
                 <span style="margin-left:5px;"> structure </span>
             </a>
@@ -142,7 +118,7 @@
 
         <li>
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+              
                <img src="backend/assets/images/Vector.svg" alt="">
                 <span style="margin-left:5px;"> Report </span>
             </a>
@@ -150,126 +126,18 @@
 
         <li>
             <a href="{{ url('/getevent') }}">
-               <!-- <i class="mdi mdi-view-dashboard-outline"></i> -->
+              
                <img src="backend/assets/images/Group.svg" alt="">
                 <span style="margin-left:5px;"> Settings  </span>
             </a>
-            <!-- <div id="layout1">
-                <div class="layout2">
-                  <h5 id="need">Need Help?</h5>
-                  <p class="open">Open our help center</p>
-                </div>
-             
-            </div> -->
+            
             <br>
               <br>
               <h4 id="fon" style="margin-left:25px;">Need Help?</h4>
               <p id="di" style="margin-left:25px;">Open our help center</p>
         </li>
 
-                            <!-- <li>
-                                <a href="#sidebarCrm" data-bs-toggle="collapse">
-                                    <i class="mdi mdi-account-multiple-outline"></i>
-                                    <span> Customer Manage   </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <div class="collapse" id="sidebarCrm">
-                                    <ul class="nav-second-level">
-                    <li>
-                        <a href="{{ route('all.customer') }}">All Customer</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('add.customer') }}">Add Customer</a>
-                    </li>
-
-                                    </ul>
-                                </div>
-                            </li> -->
-
-        <!-- <li>
-            <a href="#sidebarEmail" data-bs-toggle="collapse">
-                <i class="mdi mdi-email-multiple-outline"></i>
-                <span> Supplier Manage </span>
-                <span class="menu-arrow"></span>
-            </a>
-            <div class="collapse" id="sidebarEmail">
-                <ul class="nav-second-level">
-                    <li>
-                        <a href="{{ route('all.supplier') }}">All Supplier</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('add.supplier') }}">Add Supplier</a>
-                    </li>
-
-                </ul>
-            </div>
-        </li> -->
-
-
-
-        <!-- <li>
-            <a href="#salary" data-bs-toggle="collapse">
-                <i class="mdi mdi-email-multiple-outline"></i>
-                <span> Employee Salary </span>
-                <span class="menu-arrow"></span>
-            </a>
-            <div class="collapse" id="salary">
-                <ul class="nav-second-level">
-                    <li>
-                        <a href="{{ route('add.advance.salary') }}">Add Advance Salary</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('all.advance.salary') }}">All Advance Salary</a>
-                    </li>
-
-                     <li>
-                        <a href="{{ route('pay.salary') }}">Pay Salary</a>
-                    </li>
-
-                    <li>
-                        <a href="{{ route('month.salary') }}">Last Month Salary</a>
-                    </li>
-
-                </ul>
-            </div>
-        </li> -->
-
-
-        
-        <!-- <li>
-            <a href="#attendence" data-bs-toggle="collapse">
-                <i class="mdi mdi-email-multiple-outline"></i>
-                <span> Employee Attendence </span>
-                <span class="menu-arrow"></span>
-            </a>
-            <div class="collapse" id="attendence">
-                <ul class="nav-second-level">
-                    <li>
-                        <a href="{{ route('employee.attend.list') }}">Employee Attendence List </a>
-                    </li>
-
-                </ul>
-            </div>
-        </li>
-
- -->
-
-        <!-- <li>
-            <a href="#category" data-bs-toggle="collapse">
-                <i class="mdi mdi-email-multiple-outline"></i>
-                <span> Category </span>
-                <span class="menu-arrow"></span>
-            </a>
-            <div class="collapse" id="category">
-                <ul class="nav-second-level">
-                    <li>
-                        <a href="{{ route('all.category') }}">All Category </a>
-                    </li>
-
-                </ul>
-            </div>
-        </li> -->
-
+                        
 
          <li>
             <a href="#product" data-bs-toggle="collapse">
