9593f13dedd6f7812441d4456b349fc82c1ed248
PraviniM <pravini1105@gmail.com>
2023-10-19 16:45:29 +0530

    calendarchanges

--- resources/views/body/sidebar.blade.php
+++ resources/views/body/sidebar.blade.php
@@ -1,187 +1,170 @@
 <head>
     <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
     <style>
-   li span {
-    color: #F5F7FB;
-    font-family: Inter;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-  }
-  #side{
-    width: 260px;
-    height: 908px;
-    flex-shrink: 0;
-    border-right: 1px solid rgba(245, 247, 251, 0.20);
-    background: #1D2433;
-  }
+        li span {
+            color: #F5F7FB;
+            font-family: Inter;
+            font-size: 16px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: normal;
+        }
 
-  #change:hover{
-    background-color: #FED931;
-    border: 1px;
-    color:black;
-    border-radius: 5px;
-    margin-left:5px;
-    width: 95%;
-  }
-  #mouhov:hover{
- color:black;
-  }
+        #side {
+            width: 260px;
+            height: 908px;
+            flex-shrink: 0;
+            border-right: 1px solid rgba(245, 247, 251, 0.20);
+            background: #1D2433;
+        }
+
+        #change:hover {
+            background-color: #FED931;
+            border: 1px;
+            color: black;
+            border-radius: 5px;
+            margin-left: 5px;
+            width: 95%;
+        }
+
+        #mouhov:hover {
+            color: black;
+        }
     </style>
-   </head>
-   <div class="left-side-menu" id="side">
+</head>
+<div class="left-side-menu" id="side">
 
-                <div class="h-100" data-simplebar>
+    <div class="h-100" data-simplebar>
 
-                    <!-- User box -->
+        <!-- User box -->
 
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
 
-                    <!--- Sidemenu -->
-                    <div id="sidebar-menu">
+            <ul id="side-menu">
 
-                        <ul id="side-menu">
+                <li id="change">
+                    <a href="{{ url('/dashboard') }}">
 
-
-          <li id="change">
-            <a href="{{ url('/dashboard') }}">
-              
-               <img src="backend/assets/images/Union.svg" alt="">
-                <span style="margin-left:7px;" id="mouhov" > Dashboard </span>
-            </a>
-        </li>
+                        <img src="backend/assets/images/Union.svg" alt="">
+                        <span style="margin-left:7px;" id="mouhov"> Dashboard </span>
+                    </a>
+                </li>
 
 
-           <li id="change">
-            <a href="{{ route('pos') }}">
-               <img src="backend/assets/images/clarity_inbox-line.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Inbox </span>
-            </a>
-        </li>
+                <li id="change">
+                    <a href="{{ route('pos') }}">
+                        <img src="backend/assets/images/clarity_inbox-line.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Inbox </span>
+                    </a>
+                </li>
 
-        <li id="change">
-            <a href="{{ url('/getevent') }}">
-               <img src="backend/assets/images/Calendar.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Calendar & todo’s </span>
-            </a>
-        </li>
+                <li id="change">
+                    <a href="{{ url('/getevent') }}">
+                        <img src="backend/assets/images/Calendar.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Calendar & todo’s </span>
+                    </a>
+                </li>
 
 
 
-        <li class="nav-header recruitment mt-2" style="margin-left:23px;" >RECRUITMENT</li>
+                <li class="nav-header recruitment mt-2" style="margin-left:23px;">RECRUITMENT</li>
 
-                            <li class="mt-2" id="change">
-            <a href="{{ url('/getevent') }}">
-            
-               <img src="backend/assets/images/Bag 2.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Jobs </span>
-            </a>
-        </li>
+                <li class="mt-2" id="change">
+                    <a href="{{ url('/getevent') }}">
 
-        <li id="change">
-            <a href="{{ url('/getevent') }}">
-              
-               <img src="backend/assets/images/octicon_people-24.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Candidates  </span>
-            </a>
-        </li>
+                        <img src="backend/assets/images/Bag 2.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Jobs </span>
+                    </a>
+                </li>
 
+                <li id="change">
+                    <a href="{{ url('/getevent') }}">
 
-        <li id="change">
-            <a href="{{ url('/getevent') }}">
-             
-               <img src="backend/assets/images/ph_triangle-bold.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> My referrals </span>
-            </a>
-        </li>
+                        <img src="backend/assets/images/octicon_people-24.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Candidates </span>
+                    </a>
+                </li>
 
 
-        <li id="change">
-            <a href="{{ url('/getevent') }}">
-            
-               <img src="backend/assets/images/fluent-mdl2_site-scan.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Career site </span>
-            </a>
-        </li>
+                <li id="change">
+                    <a href="{{ url('/getevent') }}">
 
-        <li class="nav-header recruitment mt-2" style="margin-left:23px;" >ORGANIZATION </li>
+                        <img src="backend/assets/images/ph_triangle-bold.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> My referrals </span>
+                    </a>
+                </li>
 
-        <li class="mt-2" id="change">
-            <a href="{{ url('/getevent') }}">
-              
-               <img src="backend/assets/images/employee.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Employee </span>
-            </a>
-        </li>
 
-        <li id="change">
-            <a href="{{ url('/getevent') }}">
-            
-               <img src="backend/assets/images/ph_tree-structure-fill.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> structure </span>
-            </a>
-        </li>
+                <li id="change">
+                    <a href="{{ url('/getevent') }}">
 
-        <li id="change">
-            <a href="{{ url('/getevent') }}">
-              
-               <img src="backend/assets/images/Vector.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Report </span>
-            </a>
+                        <img src="backend/assets/images/fluent-mdl2_site-scan.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Career site </span>
+                    </a>
+                </li>
+
+                <li class="nav-header recruitment mt-2" style="margin-left:23px;">ORGANIZATION </li>
+
+                <li class="mt-2" id="change">
+                    <a href="{{ url('/getevent') }}">
+
+                        <img src="backend/assets/images/employee.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Employee </span>
+                    </a>
+                </li>
+
+                <li id="change">
+                    <a href="{{ url('/getevent') }}">
+
+                        <img src="backend/assets/images/ph_tree-structure-fill.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> structure </span>
+                    </a>
+                </li>
+
+                <li id="change">
+                    <a href="{{ url('/getevent') }}">
+
+                        <img src="backend/assets/images/Vector.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Report </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ url('/getevent') }}" id="change">
+
+                        <img src="backend/assets/images/Group.svg" alt="">
+                        <span style="margin-left:5px;" id="mouhov"> Settings </span>
+                    </a>
+
+                    <br>
+                    <br>
+                    <h4 id="fon" style="margin-left:25px;">Need Help?</h4>
+                    <p id="di" style="margin-left:25px;">Open our help center</p>
+                </li>
+            </ul>
+        </div>
         </li>
 
         <li>
-            <a href="{{ url('/getevent') }}" id="change">
-              
-               <img src="backend/assets/images/Group.svg" alt="">
-                <span style="margin-left:5px;" id="mouhov"> Settings  </span>
+            <a href="#sidebarExpages" data-bs-toggle="collapse">
+                <i class="mdi mdi-text-box-multiple-outline"></i>
+                <span> Extra Pages </span>
+                <span class="menu-arrow"></span>
             </a>
-            
-            <br>
-              <br>
-              <h4 id="fon" style="margin-left:25px;">Need Help?</h4>
-              <p id="di" style="margin-left:25px;">Open our help center</p>
-        </li>
-
-
-                           
-
-        </ul>
-                                </div>
-                            </li>
-
-                            <li>
-                                <a href="#sidebarExpages" data-bs-toggle="collapse">
-                                    <i class="mdi mdi-text-box-multiple-outline"></i>
-                                    <span> Extra Pages </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <div class="collapse" id="sidebarExpages">
-                                    <ul class="nav-second-level">
-                                        <li>
-                                            <a href="pages-starter.html">Starter</a>
-                                        </li>
-                                        <li>
-                                            <a href="pages-timeline.html">Timeline</a>
-                                        </li>
-                                        <li>
-                                            <a href="pages-sitemap.html">Sitemap</a>
-                                        </li>
-
-                                    </ul>
-                                </div>
-                            </li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-
-                    </div>
-                   
-
-                    <div class="clearfix"></div> 
-                </div>
-                <!-- Sidebar -left-->
-
-
+            <div class="collapse" id="sidebarExpages">
+                <ul class="nav-second-level">
+                    <li>
+                        <a href="pages-starter.html">Starter</a>
+                    </li>
+                </ul>
             </div>
+        </li>
+    </div>
+
+</div>
+
+<div class="clearfix"></div>
+</div>
+<!-- Sidebar -left-->
+</div>
--- resources/views/calendar/fullcalendar.blade.php
+++ resources/views/calendar/fullcalendar.blade.php
@@ -1,99 +1,229 @@
-
 <!-- Other head elements -->
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <title>Laravel 9 Fullcalandar Jquery Ajax Create and Delete Event </title>
-  <link href='https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css' rel='stylesheet'>
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.5.1/fullcalendar.min.css" />
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.5.1/fullcalendar.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css" rel="stylesheet" />
+<!-- JS for jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- JS for full calender -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+<!-- bootstrap css and js -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+<link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
+<link href="{{ asset('vendors/fullcalendar/css/fullcalendar.css') }}" rel="stylesheet" type="text/css" />
+
 
 <style>
   .recruitment {
-color: #9099AD;
-font-family: Inter;
-font-size: 14px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-}
+    color: #9099AD;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
 </style>
 
 </head>
 <center>
-    <div style="text-align: right;">
-        <button   class="btn btn-red" onclick="goToAllEvents()">
-          <i class="ri-account-circle-line"></i>
-          <span>All events</span>
-        </button>
-      </div>
+  <div style="text-align: right;">
+    <button class="btn btn-red" onclick="goToAllEvents()">
+      <i class="ri-account-circle-line"></i>
+      <span>All events</span>
+    </button>
+  </div>
 </center>
 <script>
-    function goToAllEvents() {
-        // Replace 'route-url' with the actual URL of the 'allevents' route
-        var routeURL = 'allevent';
-        window.location.href = routeURL;
-    }
+  function goToAllEvents() {
+    // Replace 'route-url' with the actual URL of the 'allevents' route
+    var routeURL = 'allevent';
+    window.location.href = routeURL;
+  }
 </script>
 
 <body>
-<div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h2>Calendar</h2>
-                <div id="calendar">
-                    <div class="model-body" id="calmodel">
-
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#basicExampleModal">
+    View Demo
+  </button>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Calendar</h2>
+        <div id="calendar"></div>
+        <!-- Modal -->
+        <div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+          aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Appointment Details</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="mb-3">
+                  <label class="form-label">Title</label>
+                  <input name="title" class="form-control" type="text" value="" id="example-text-input">
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Start Event</label>
+                  <input name="link_url" class="form-control" type="text" value="" id="example-text-input">
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">End Event</label>
+                  <input name="link_url" class="form-control" type="text" value="" id="example-text-input">
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Interview Call Link</label>
+                  <input name="link_url" class="form-control" type="url" value="" id="example-text-input">
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Candidate Profile</label>
+                  <input name="link_url" class="form-control" type="file" value="" id="example-text-input">
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Candidate Name</label>
+                  <input name="link_url" class="form-control" type="text" value="" id="example-text-input">
+                </div>
+                <div class="mb-3">
+                  <div class="card">
+                    <div class="card-body">
+                      <p>Score Card</p>
+                      <hr>
+                      <h5>Interpesronal Skills</h5>
+                      <div class="mb-3">
+                        <input name="link_url" class="form-control" type="text" value=" onle temporiely"
+                          id="example-text-input" style="border:none">
+                      </div>
+                      <hr>
+                      <h5>Communication Skills</h5>
+                      <div class="mb-3">
+                        <input name="link_url" class="form-control" type="text" value=" onle temporiely"
+                          id="example-text-input" style="border:none">
+                      </div>
+                      <hr>
+                      <h5>Problem solving</h5>
+                      <div class="mb-3">
+                        <input name="link_url" class="form-control" type="text" value=" onle temporiely"
+                          id="example-text-input" style="border:none">
+                      </div>
                     </div>
+                  </div>
                 </div>
+                <div class="mb-3">
+                  <label class="form-label">HR Profile</label>
+                  <input name="link_url" class="form-control" type="file" value="" id="example-text-input">
                 </div>
+                <div class="mb-3">
+                  <label class="form-label">HR Name</label>
+                  <input name="link_url" class="form-control" type="url" value="" id="example-text-input">
+                </div>
+                <div class="mb-3">
+                  <label class="form-label"> HR Email ID</label>
+                  <input name="link_url" class="form-control" type="email" value="" id="example-text-input">
+                </div>
+                <hr>
+                <div class="mb-3">
+                  <label class="form-label">Instruction</label>
+                  <input name="link_url" class="form-control" type="url" value="" id="example-text-input">
+                </div>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save Details</button>
+              </div>
             </div>
-
+          </div>
         </div>
-
+      </div>
     </div>
+  </div>
 </body>
-
-
 <script>
-    $(document).ready(function () {
-        var calendar = $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,basicWeek,basicDay'
-            },
-            navLinks: true,
-            editable: true,
-            events: "getevent",          
-            displayEventTime: false,
-            eventRender: function (event, element, view) {
-                if (event.allDay === 'true') {
-                    event.allDay = true;
-                } else {
-                    event.allDay = false;
-                }
-            },
-        selectable: true,
-        selectHelper: true,
-        select: function (start, end, allDay) {
-                var title = prompt('Event Title:');
-            if (title) {
-                var start = moment(start, 'DD.MM.YYYY').format('YYYY-MM-DD');
-                var end = moment(end, 'DD.MM.YYYY').format('YYYY-MM-DD');
-                $.ajax({
-                    url: "{{ URL::to('createevent') }}",
-                    data: 'title=' + title + '&start=' + start + '&end=' + end +   '&_token=' +"{{ csrf_token() }}" ,
-                    type: "post",
-                    success: function (data) {
-                        alert("Added Successfully");
-                        $('#calendar').fullCalendar('refetchEvents');
-                    }
-                });
-            }
-        },
 
-        });
-    });
+  // $('#calendar').fullCalendar({
+  //     header: {
+  //         left: 'prev,next today',
+  //         center: 'title',
+  //         right: 'month,agendaWeek,agendaDay,listWeek'
+  //     },
+  //     defaultDate: '2018-11-16',
+  //     navLinks: true,
+  //     eventLimit: true,
+  //     events: [{
+  //             title: 'Front-End Conference',
+  //             start: '2018-11-16',
+  //             end: '2018-11-18'
+  //         },
+  //         {
+  //             title: 'Hair stylist with Mike',
+  //             start: '2018-11-20',
+  //             allDay: true
+  //         },
+  //         {
+  //             title: 'Car mechanic',
+  //             start: '2018-11-14T09:00:00',
+  //             end: '2018-11-14T11:00:00'
+  //         },
+  //         {
+  //             title: 'Dinner with Mike',
+  //             start: '2018-11-21T19:00:00',
+  //             end: '2018-11-21T22:00:00'
+  //         },
+  //         {
+  //             title: 'Chillout',
+  //             start: '2018-11-15',
+  //             allDay: true
+  //         },
+  //         {
+  //             title: 'Vacation',
+  //             start: '2018-11-23',
+  //             end: '2018-11-29'
+  //         },
+  //     ]
+  // });
+
+  $(document).ready(function () {
+    var calendar = $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,basicWeek,basicDay'
+      },
+      navLinks: true,
+      editable: true,
+      events: "getevent",
+      displayEventTime: false,
+      eventRender: function (event, element, view) {
+        if (event.allDay === 'true') {
+          event.allDay = true;
+        } else {
+          event.allDay = false;
+        }
+      },
+      selectable: true,
+      selectHelper: true,
+      select: function (start, end, allDay) {
+        var title = prompt('Event Title:');
+        if (title) {
+          var start = moment(start, 'DD.MM.YYYY').format('YYYY-MM-DD');
+          var end = moment(end, 'DD.MM.YYYY').format('YYYY-MM-DD');
+          $.ajax({
+            url: "{{ URL::to('createevent') }}",
+            data: 'title=' + title + '&start=' + start + '&end=' + end + '&_token=' + "{{ csrf_token() }}",
+            type: "post",
+            success: function (data) {
+              alert("Added Successfully");
+              $('#calendar').fullCalendar('refetchEvents');
+            }
+          });
+        }
+      },
+
+    });
+  });
 </script>
-
