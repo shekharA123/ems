d8c14c1bd3c29e940dd440bb30308a29c513c5dd
PraviniM <pravini1105@gmail.com>
2023-11-23 18:03:26 +0530

    updated

--- database/migrations/2023_08_05_130309_create_events_table.php
+++ database/migrations/2023_08_05_130309_create_events_table.php
@@ -16,6 +16,16 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->string('link_url');
+            $table->string('candidate_profile');
+            $table->string('candidate_name');
+            $table->string('interpersonal_skill');
+            $table->string('communication_skill');
+            $table->string('problem_sovling');
+            $table->string('hr_profile');
+            $table->string('hr_name');
+            $table->string('hr_email');
+            $table->string('instruction');
             $table->dateTime('start');
             $table->dateTime('end');
             $table->timestamps();
