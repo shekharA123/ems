95e8ba13ed86f3375ffaef5fb8ede7839f7ea155
PraviniM <pravini1105@gmail.com>
2023-12-01 19:23:29 +0530

    modifyenv

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,22 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'title',
-        'link_url',
-        'candidate_profile',
-        'candidate_name',
-        'interpersonal_skill',
-        'communication_skill',
-        'problem_sovling',
-        'hr_profile',
-        'hr_name',
-        'hr_email',
-        'instruction',
-        'start',
-        'end',
-        'created_at',
+    protected $guarded = [];
+    // protected $fillable = [
+    //     'title',
+    //     'link_url',
+    //     'candidate_profile',
+    //     'candidate_name',
+    //     'interpersonal_skill',
+    //     'communication_skill',
+    //     'problem_sovling',
+    //     'hr_profile',
+    //     'hr_name',
+    //     'hr_email',
+    //     'instruction',
+    //     'start',
+    //     'end',
+    //     'created_at',
         
-    ];
+    // ];
   
 }
